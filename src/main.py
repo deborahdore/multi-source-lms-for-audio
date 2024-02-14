e9afd29315382e0b5903195e531362444e28c8af
Deborah Dore <deborah.dore@outlook.com>
2024-02-14 14:46:49 +0100

    add plots

--- src/main.py
+++ src/main.py
@@ -227,8 +227,8 @@ def main(cfg: DictConfig):
 	if cfg.train_bert:
 		metric_dict, _ = train_bert(cfg)
 
-	generate(cfg)
-	visualize(cfg)
+	# generate(cfg)
+	# visualize(cfg)
 
 	# safely retrieve metric value for hydra-based hyperparameter optimization
 	metric_value = get_metric_value(metric_dict=metric_dict, metric_name=cfg.get("optimized_metric"))
